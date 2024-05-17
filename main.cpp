7c31ebf6aec22aa70df40c38b53dbe24d5450759
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-05-17 15:20:24 +0900

    01_00完成

--- main.cpp
+++ main.cpp
@@ -5,15 +5,81 @@
 
 const char kWindowTitle[] = "LE2C_16_タカキ_ケンゴ_MT3";
 
+// 投資投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fov, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 matrix = {};
+	float tanHalfFov = tanf(fov / 2.0f);
+
+	matrix.m[0][0] = 1.0f / (aspectRatio * tanHalfFov);
+	matrix.m[1][1] = 1.0f / tanHalfFov;
+	matrix.m[2][2] = farClip / (farClip - nearClip);
+	matrix.m[2][3] = 1.0f;
+	matrix.m[3][2] = -(farClip * nearClip) / (farClip - nearClip);
+	matrix.m[3][3] = 0.0f;
+
+	return matrix;
+}
+
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 matrix = {};
+
+	matrix.m[0][0] = 2.0f / (right - left);
+	matrix.m[1][1] = 2.0f / (top - bottom);
+	matrix.m[2][2] = 1.0f / (farClip - nearClip);
+	matrix.m[3][0] = -(right + left) / (right - left);
+	matrix.m[3][1] = -(top + bottom) / (top - bottom);
+	matrix.m[3][2] = -nearClip / (farClip - nearClip);
+	matrix.m[3][3] = 1.0f;
+
+	return matrix;
+}
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 matrix = {};
+
+	matrix.m[0][0] = width / 2.0f;
+	matrix.m[1][1] = -height / 2.0f;
+	matrix.m[2][2] = maxDepth - minDepth;
+	matrix.m[3][0] = left + width / 2.0f;
+	matrix.m[3][1] = top + height / 2.0f;
+	matrix.m[3][2] = minDepth;
+	matrix.m[3][3] = 1.0f;
+
+	return matrix;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; row++) {
+		for (int column = 0; column < 4; column++) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Matrix4x4 orthographicMatrix =
+		MakeOrthographicMatrix(-160.f, 160.f, 200.f, 300.f, 0.0f, 1000.0f);
+
+	Matrix4x4 perspectiveFovMatrix =
+		MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+
+	Matrix4x4 viewportMatrix =
+		MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -35,6 +101,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveForvMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
