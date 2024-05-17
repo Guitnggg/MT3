55021c5f6c934c8b76e4233dcf304fa6e831efa3
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-05-17 14:27:14 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <Novice.h>
+#include <Vector3.h>
+#include <Matrix4x4.h>
+#include <cmath>
 
 const char kWindowTitle[] = "LE2C_16_タカキ_ケンゴ_MT3";
+
+// 
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+// 
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+// 
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	std::cos(radian);
+
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
