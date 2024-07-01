01a38054e2d950e2337568db53a7662bd60df661
LC1D_14_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-07-02 00:11:16 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,88 @@
 #include <Novice.h>
+#include <Vector3.h>
+#include "Matrix4x4.h"
+#include <cmath>
+#include <imgui.h>
 
 const char kWindowTitle[] = "LE2C_16_タカキ_ケンゴ_MT3";
+
+struct Sphere
+{
+	Vector3 center;  // 中心点
+	float radius;  // 半径
+};
+
+//===============
+// グリッドを表示する関数
+//===============
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix)
+{
+	const float kGridHalfWidth = 2.0f;
+	const uint32_t kSubdivision = 10;
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);
+
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
+	{
+		float x = -kGridHalfWidth + (xIndex * kGridEvery);
+		unsigned int color = 0xAAAAAAFF;
+		
+		Vector3 start{ x,0.0f,-kGridHalfWidth };
+		Vector3 end{ x,0.0f,kGridHalfWidth };
+
+		Vector3 startScreen = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 endScreen = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		if (x == 0.0f)
+		{
+			color = BLACK;
+		}
+
+		Novice::DrawLine(int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y), color);
+	}
+
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex)
+	{
+		float z = -kGridHalfWidth + (zIndex * kGridEvery);
+		unsigned int color = 0xAAAAAAFF;
+
+		Vector3 start{ z,0.0f,-kGridHalfWidth };
+		Vector3 end{ z,0.0f,kGridHalfWidth };
+
+		Vector3 startScreen = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 endScreen = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		if (z == 0.0f)
+		{
+			color = BLACK;
+		}
+
+		Novice::DrawLine(int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y), color);
+	}
+}
+
+//===============
+// 球体を表示する関数
+//===============
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+	const uint32_t kSubdivision = 32;  // 分割数
+	const float kLonEvery = 20.0f;  // 経度分割１つ分の角度
+	const float kLatEvery = 20.0f;  // 緯度分割１つ分の角度
+
+	// 緯度の方向に分割　-π/2 ~ π/2
+	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex)
+	{
+		float lat = -pi / 2.0f + kLatEvery * latIndex;  // 現在の緯度
+		// 
+		Vector3 a, b, c;
+		// 
+		// 
+		Novice::DrawLine();
+	}
+}
+
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,8 +91,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,6 +106,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		ImGui::Begin("WIndow");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.0f);
 
 		///
 		/// ↑更新処理ここまで
